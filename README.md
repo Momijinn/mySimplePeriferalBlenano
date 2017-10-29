abc5b8c955b7f6d2e6e520bf0fe7f258a44a5896
KanameTakano <momijinn.aka@gmail.com>
2017-10-29 15:09:09 +0900

    initialize commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SampleBlenano2BlePeripheral
 Blenano2をble通信のPeripheralとして使うサンプルプログラム
+
+## 環境
+Blenano2をArduino上で使用できるようにしています
+
+Arduino化する方法は下記を参照
+
+https://github.com/redbear/nRF5x/blob/master/nRF52832/docs/Arduino_Board_Package_Installation_Guide.md
+
+# プログラム
+Blenano2はPeripheralとして動作し、2秒毎に6つのデータ(tx_buf)を送信しています
+
+* ticker2s.attach(task_handle, 2);
+
+    attachの値変えれば更新頻度を変えることができます
+
+*  ble.updateCharacteristicValue();
+
+    Timer関数のなかにある下記のプログラムにてNotifyの更新を行っています
---
+++ mySimplePeriferalBlenano2/mySimplePeriferalBlenano2.ino
@@ -0,0 +1,116 @@
+#include <nRF5x_BLE_API.h>
+#define TXRX_BUF_LEN 20
+#define DEVICE_NAME  "MyBlePeripheral"
+
+BLE ble;
+
+//Timer
+Ticker ticker2s;
+
+//データの格納する配列
+//uint8_t = 0~255
+static uint8_t tx_buf[TXRX_BUF_LEN];
+
+// The Nordic UART Service
+static const uint8_t uart_base_uuid_rev[]   = {0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71};
+static const uint8_t service_uuid[]        = {0x71, 0x3D, 0, 0, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+static const uint8_t service_chars_uuid[] = {0x71, 0x3D, 0, 1, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E};
+
+uint8_t chars_value[TXRX_BUF_LEN] = {0}; //初期化
+
+//Notifyのみもらう
+GattCharacteristic characteristic(service_chars_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
+GattCharacteristic *uartChars[] = {&characteristic};
+GattService uartService(service_uuid, uartChars, sizeof(uartChars) / sizeof(GattCharacteristic *));
+
+
+//connect task
+void connectionCallBack( const Gap::ConnectionCallbackParams_t *params ) {
+  Serial.println("Conect");
+   if(params->role == Gap::PERIPHERAL) {
+    Serial.println("Peripheral ");
+  }
+}
+
+//disconect tack
+void disconnectionCallBack(const Gap::DisconnectionCallbackParams_t *params) {
+  Serial.print("Disconnected hande : ");
+  Serial.println(params->handle);
+  Serial.print("Disconnected reason : ");
+  Serial.println(params->reason);
+  Serial.println("Restart advertising ");
+  ble.startAdvertising();
+}
+
+//Timer Task(Notify Task)
+void task_handle(void) {
+  tx_buf[0] += 1;
+  tx_buf[1] += 1;
+  tx_buf[2] += 1;
+  tx_buf[3] += 1;
+  tx_buf[4] += 1;
+  tx_buf[5] += 1;
+
+  //Send Notify Data
+  ble.updateCharacteristicValue(characteristic.getValueAttribute().getHandle(), tx_buf, 6);
+}
+
+
+void SetAdvertisement(){
+  // A list of Advertising Data types commonly used by peripherals.
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+  // Add short name to advertisement
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::SHORTENED_LOCAL_NAME,(const uint8_t *)"TXRX", sizeof("TXRX") - 1);
+  // Add complete 128bit_uuid to advertisement
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,(const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid_rev));
+  // Add complete device name to scan response data
+  ble.accumulateScanResponse(GapAdvertisingData::COMPLETE_LOCAL_NAME,(const uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME) - 1);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  //BLE設定
+  ble.init();
+  ble.onConnection(connectionCallBack);
+  ble.onDisconnection(disconnectionCallBack);
+
+  //set advertisement
+  SetAdvertisement();
+
+  // set adv_type
+  ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+  // add service
+  ble.addService(uartService);
+  // set device name
+  ble.setDeviceName((const uint8_t *)DEVICE_NAME);
+  // set tx power,valid values are -40, -20, -16, -12, -8, -4, 0, 4
+  ble.setTxPower(4);
+  // set adv_interval, 100ms in multiples of 0.625ms.
+  ble.setAdvertisingInterval(160);
+  // set adv_timeout, in seconds
+  ble.setAdvertisingTimeout(0);
+  // start advertising
+  ble.startAdvertising();
+
+  // ger BLE stack version
+  Serial.print("BLE stack verison is : ");
+  Serial.println(ble.getVersion());
+
+
+  //Initialize Data
+  tx_buf[0] = 0;
+  tx_buf[1] = 0;
+  tx_buf[2] = 0;
+  tx_buf[3] = 0;
+  tx_buf[4] = 0;
+  tx_buf[5] = 0;
+
+  // Start Task
+  //2 sec
+  ticker2s.attach(task_handle, 2);
+}
+
+void loop() {
+  ble.waitForEvent();
+}
